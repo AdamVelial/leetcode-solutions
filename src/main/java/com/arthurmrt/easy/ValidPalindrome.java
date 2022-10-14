27c69674b4bdb3094dd2fa3f2a2b2cf972b5e31f
Arthur Mt <arthurmt@yandex.ru>
2022-10-14 11:34:33 +0300

    125. Valid Palindrome update

--- src/main/java/com/arthurmrt/easy/ValidPalindrome.java
+++ src/main/java/com/arthurmrt/easy/ValidPalindrome.java
@@ -2,7 +2,30 @@ package com.arthurmrt.easy;
 
 // 125. Valid Palindrome
 public class ValidPalindrome {
+
+    // second solution
     public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+
+        while (left < right) {
+            if ( !Character.isLetterOrDigit(s.charAt(left)) ) {
+                left++;
+            } else if (!Character.isLetterOrDigit( s.charAt(right)) ) {
+                right--;
+            } else if ( Character.toLowerCase( s.charAt(left) ) != Character.toLowerCase( s.charAt(right)) ){
+                return false;
+            } else {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+
+    // first solution
+    public boolean isPalindrome_Old(String s) {
         String str = s.trim().replaceAll("[^A-Za-z0-9]","").toLowerCase();
         if (str.isEmpty()) return true;
 
